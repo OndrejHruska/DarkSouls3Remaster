d40ad266aed3efe2ac285fea220569b7c8e6b153
OndrejHruska <145443228+OndrejHruska@users.noreply.github.com>
2024-01-16 13:10:18 +0100

    Add files via upload

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Runtime.Remoting.Messaging;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+
+        //BDD = chováním řízený vývoj
+        //TDD = testování řízen projekt
+        public void TestMethodPos()
+        {
+            /*podmínky pro úspěch aplikace*/
+        }
+        public void TestMethodNeg()
+        {
+        }
+    
+    }
+}
